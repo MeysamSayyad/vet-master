7f7efcb34c1b6d6f3894a3d4e9191a1c3e334e1f
m2y2m <meysamsayyadtala@gmail.com>
2023-07-29 19:00:48 +0330

    histogram chart added

--- src/routes/vet/ViewV/V_SeedV.js
+++ src/routes/vet/ViewV/V_SeedV.js
@@ -1,11 +1,87 @@
-import React from 'react'
-
+import React,{useEffect, useState} from 'react'
+import Fetch from '../../../components/Fetch'
+import { AgChartsReact } from 'ag-charts-react';
+import { useNavigate, useParams } from 'react-router-dom';
 const V_SeedV = () => {
+const [loading,setLoading]=useState(true)
+const [data,setdata]=useState([])
+const params=useParams()
+const navigate=useNavigate()
+const [options, setOptions] = useState({})
+console.log(data)
+console.log(options)
+  useEffect(()=>{
+    const body=undefined
+    const token=true
+    const method='GET'
+    const api=`/api/v1/feed/?epoch_id=${params.EpochId}`
+    Fetch(body,token,setdata,method,api,navigate,setLoading)
+  },[])
+  useEffect(()=>{
+    if(data.length >0){
 
+   
+    const ndata=data.map((item)=>{
+      return {...item,amount:item.amount*1,mean:(item.start_age+ item.end_age)/2}
+    })
+    const bins=data.map((i)=> [i.start_age,i.end_age] )
+    console.log(bins)
+setOptions(
+  {
+    data:ndata
+      ,
+      tooltip: {
+        enabled: true,
+        showArrow: true,
+        class:'tooltipfont'
+      },
+    series: [
+      {
+        fillOpacity:0.2,
+        type: 'histogram',
+        xKey: 'mean',
+        xName: 'میانگین سنی',
+        yKey: 'amount',
+        yName: 'مقدار دان مصرفی',
+        fontSize:20,
+        aggregation: 'mean',
+        fill:"#1984c5",
+        stroke:"#1984c5",
+        highlightStyle: {
+          item: {
+            fill: '#1984c5',
+            fillOpacity:0.7
+          }
+        }
+        
+      },
+    ],
+    axes: [
+      {
+        type: 'number',
+        position: 'bottom',
+        title: { text: 'سن',fontSize:16 },
+        tick: { interval: 2 },
+        min:0
+      },
+      {
+        type: 'number',
+        position: 'left',
+        title: { text: 'میانگین دان مصرفی' ,fontSize:16  ,fontWeight:'normal' },
+      },
+    ],
+  }
+)
+}
+  },[data])
+    
 // if(data.length === 0)return <h1 className="text"> اطلاعاتی هنوز ثبت نشده</h1>
-return (
-<div>
-  <h1 className="text"> اطلاعاتی هنوز ثبت نشده</h1>
-</div>
+return loading ? <div className='flex justify-center items-center'> <div className=' border-2 border-gray-700  w-8 h-8 border-r-transparent animate-spin  rounded-full '> </div></div> : 
+data.length === 0 ? <h1 className="text"> اطلاعاتی هنوز ثبت نشده</h1>:(
+
+  <div>
+    <h3 className=' text-center text-xl font-bold'>نمودار دان مصرفی -سن</h3>
+    {options.data  &&<AgChartsReact options={options} />}
+  </div>
 )}
 export default V_SeedV
