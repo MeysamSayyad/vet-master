05fb0ce558fa425bb1b7d2952a7e655f626ac21e
m2y2m <meysamsayyadtala@gmail.com>
2023-07-30 10:55:57 +0330

    V_EndCourse conneted

--- src/routes/vet/ViewV/V_EndCourse.js
+++ src/routes/vet/ViewV/V_EndCourse.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import { useContext } from 'react';
 import { myContext } from '../../../context';
+import { useNavigate, useParams } from 'react-router-dom';
+import Fetch from '../../../components/Fetch';
 
 
 
 const V_EndCourse = () => {
-  const {epoch} = useContext(myContext);
+  const [data,setdata]=useState([])
+  const {epoch,access,refresh} = useContext(myContext);
+  const [loading,setLoading]=useState(true)
+  const navigate=useNavigate()
+  const params=useParams()
 
-return (
+  useEffect(()=>{
+
+if(!epoch.is_active)    {
+    const body=undefined
+    const token=true
+    const method='GET'
+    const api=`/api/v1/epochs/final-stats/?epoch_id=${params.EpochId}`
+    Fetch(body,token,setdata,method,api,navigate,setLoading,undefined,undefined,access,refresh)}
+    else{
+      setLoading(false)
+    }
+  },[])
+
+return loading ? <div className='flex justify-center items-center'> 
+<div className=' border-2 border-gray-700  w-8 h-8 border-r-transparent animate-spin  rounded-full '> </div></div>:(
 <div className="text-xl mt-4">
     {epoch.is_active ? 
     "تا پایان دوره امکان مشاهده این قسمت امکان پذیر نمی باشد.":
@@ -17,7 +37,7 @@ return (
         <h4 className="mt-4 drop-shadow-sm"> شاخص تولیدی مرغ گوشتی :</h4>
       </div>
       <div dir='ltr' className="">
-        <p className="mt-4 drop-shadow-sm">{0} </p>
+        <p className="mt-4 drop-shadow-sm">{data.herd_survival_percentage} </p>
         <p className="mt-4 drop-shadow-sm">{0} </p>
       </div>
     </div>
